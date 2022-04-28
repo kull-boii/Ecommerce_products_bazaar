ce2a9e4fce34081b0cc33b2a4b8f1a55d9f85258
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-04-29 02:54:57 +0530

    added components

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
-  )
-}
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/prodcut/ID">
+            <button type="button">Button text</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
